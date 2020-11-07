9d8b95cb2c2756743c36d812ae0c2e3bbb78a07e
Arthur Lafrance <lafrancearthur@gmail.com>
2020-11-06 19:29:42 -0800

    initial infrastructure setup

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "config.h"
+
+#include <iostream>
+
+int main() {
+    std::cout << "v" << VERSION << std::endl;
+    return 0;
+}
---
+++ target/CMakeFiles/botnet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/botnet.dir/main.cpp.o"
+  "botnet"
+  "botnet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/botnet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
